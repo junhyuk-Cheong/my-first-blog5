87741ba35549b566dd195aef7ebb305fe545a27f
junhyuk-Cheong <wings9701jh@gmail.com>
2020-09-08 08:21:45 +0900

    haha

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
+
+def map(request):
+    return render(request. 'blog/map.html', {})
+
+
 # Create your views here.
